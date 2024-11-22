8268e1c4cd97881e94c70d43cc6dbe9aaff3325e
Emanuel Gauler <emanuelgauler@gmail.com>
2024-11-22 12:29:44 -0300

    se quitó la clase TaskGateway

--- src/core/index.js
+++ src/core/index.js
@@ -1,56 +1,6 @@
-import { file, write } from "bun"
-import { join } from 'path'
 import { error, log } from 'console'
+import { TaskGateway } from "../infra/TaskGateway"
 
-const TASKS_PATH = join(process.cwd(), "tasks.json") // Replace with your actual path to your tasks file
-
-class TaskGateway {
-    static tasks_file = file(TASKS_PATH, { type: 'application/json' })
-    static async init() {
-        // Connect to your database here
-        // Return an instance of your TaskGateway class
-        const exists = await this.tasks_file.exists()
-        if( exists ) {
-            const content = await this.tasks_file.json()
-            return new TaskGateway(content)
-        } else {
-            const content = []
-            await write(this.tasks_file, JSON.stringify(content))
-            return new TaskGateway(content)
-        }
-    }
-
-    constructor(tasks) {
-        this.tasks = tasks || []
-    }
-
-    find_by(id) {
-        return this.tasks.find( e => e.id == id )
-    }
-
-    async find_from(filter) {
-        // Implementation here
-        // This function should connect to your database, fetch tasks that match the given filter, and return them
-        return this.tasks.filter(task => task.status.includes(filter))
-    }
-
-    async insert( task ) {
-        this.tasks.push( task )
-        await write( TaskGateway.tasks_file, JSON.stringify(this.tasks))
-    }
-
-    async mark( status, id ) {
-        const task = this.tasks.find( e => e.id == id )
-        if( task ) {
-            task.status = status
-            task.updated_at = new Date()
-            await write( TaskGateway.tasks_file, JSON.stringify(this.tasks))
-        } else {
-            throw new Error("task not found", { cause: "TaskNotFound" })
-        }
-    }
-}
-const gateway = await TaskGateway.init()
 
 function id_generator() {
     return Math.floor(Math.random() * 10 ** 7)
@@ -63,6 +13,7 @@ export async function get_all_tasks(...params) {
     // Implementation here
     // This function should connect to your database, fetch all tasks, and return them
     const [ filter ] = params
+    const gateway = await TaskGateway.init()
     return await gateway.find_from( filter )
 }
 
@@ -77,6 +28,7 @@ export async function add_task_with( description ) {
         , created_at: today
         , updated_at: today
     }
+    const gateway = await TaskGateway.init()
     gateway.insert( new_task )
     return new_task
 }
