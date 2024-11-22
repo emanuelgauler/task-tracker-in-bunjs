e864ebb4262feeccdb49ceeecfc0cb938698a3b9
Emanuel Gauler <emanuelgauler@gmail.com>
2024-11-22 12:30:12 -0300

    se separó la clase para manejar el JSON

---
+++ src/infra/TaskGateway.js
@@ -0,0 +1,51 @@
+import { file, write } from "bun";
+import { join } from "path/posix";
+
+const TASKS_PATH = join(process.cwd(), "tasks.json"); // Replace with your actual path to your tasks file
+
+export class TaskGateway {
+    static tasks_file = file(TASKS_PATH, { type: 'application/json' });
+    static async init() {
+        // Connect to your database here
+        // Return an instance of your TaskGateway class
+        const exists = await this.tasks_file.exists();
+        if (exists) {
+            const content = await this.tasks_file.json();
+            return new TaskGateway(content);
+        } else {
+            const content = [];
+            await write(this.tasks_file, JSON.stringify(content));
+            return new TaskGateway(content);
+        }
+    }
+
+    constructor(tasks) {
+        this.tasks = tasks || [];
+    }
+
+    find_by(id) {
+        return this.tasks.find(e => e.id == id);
+    }
+
+    async find_from(filter) {
+        // Implementation here
+        // This function should connect to your database, fetch tasks that match the given filter, and return them
+        return this.tasks.filter(task => task.status.includes(filter));
+    }
+
+    async insert(task) {
+        this.tasks.push(task);
+        await write(TaskGateway.tasks_file, JSON.stringify(this.tasks));
+    }
+
+    async mark(status, id) {
+        const task = this.tasks.find(e => e.id == id);
+        if (task) {
+            task.status = status;
+            task.updated_at = new Date();
+            await write(TaskGateway.tasks_file, JSON.stringify(this.tasks));
+        } else {
+            throw new Error("task not found", { cause: "TaskNotFound" });
+        }
+    }
+}
